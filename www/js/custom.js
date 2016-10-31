5725e28a0e4d5d804c819ee37bc31f0504d700d1
DavidWallJ <djkindread@gmail.com>
2016-10-31 21:18:04 +0800

    fixed back button spell doubling

--- www/js/custom.js
+++ www/js/custom.js
@@ -58,6 +58,7 @@ $(document).on("pageshow","#index2",function(){ // When entering pagetwo
     $("#btnOrderByRange").click(showRecordsByRange);
     $("#btnOrderByName").click(showRecords);
     $("#btnOrderByDuration").click(showRecordsByDuration);
+    $("#spellBookBackButton").click(dropTable);
     // $("#btnDrop").click(dropTable);
 
 
@@ -100,7 +101,7 @@ $(document).on("pageshow","#index2",function(){ // When entering pagetwo
         sortByDurationStatement = 'SELECT * FROM spell WHERE ' + className + ' = 1 AND level = ' + classLevel + ' ORDER BY CASE WHEN duration = "Special" THEN 0 WHEN duration = "Until dispelled" THEN 5 WHEN duration = "Until dispelled or triggered" THEN 7 WHEN duration = "30 days" THEN 10 WHEN duration = "10 days" THEN 15 WHEN duration = "7 days" THEN 20 WHEN duration = "1 day" THEN 25 WHEN duration = "CON, up to 1 day" THEN 27 WHEN duration = "24 hours" THEN 30 WHEN duration = "CON, up to 24 hours" THEN 35 WHEN duration = "8 hours" THEN 40 WHEN duration = "CON, up to 8 hours" THEN 55 WHEN duration = "Up to 8 hours" THEN 50 WHEN duration = "CON, up to 2 hours" THEN 58 WHEN duration = "1 hour" THEN 60 WHEN duration = "Instantaneous or 1 hour" THEN 62 WHEN duration = "CON, up to 1 hour" THEN 68 WHEN duration = "Up to 1 hour" THEN 65 WHEN duration = "10 minutes" THEN 70 WHEN duration = "CON, up to 10 minutes" THEN 75 WHEN duration = "1 minute" THEN 85 WHEN duration = "CON, up to 1 minute" THEN 80 WHEN duration = "Up to 1 minute" THEN 87 WHEN duration = "CON, up to 6 rounds" THEN 88 WHEN duration = "1 round" THEN 90 WHEN duration = "CON, up to 1 round" THEN 92 WHEN duration = "Instantaneous" THEN 95 ELSE 200 END';
     }
 
-    
+
     //add active class to index2 nav
     $('.spellSortNav ul li button').click(function(e) {
         $('.spellSortNav ul li button').removeClass('ui-btn-active');
